0e95197bfbf453dd53b566e34a1d53fd6f6146c6
alllex123456 <alextanase454@gmail.com>
2022-02-21 17:57:32 +0200

    added quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
 export default function AllQuotes() {
-  return (
-    <section>
-      <h1>Quotes List</h1>
-    </section>
-  );
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 }
